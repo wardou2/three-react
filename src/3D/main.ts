fc7fc6c702c1c559b596e3f21907c798f9b11a39
wardou2 <ward92@gmail.com>
2020-08-31 16:37:10 -0400

    Move initialization into separate functions for clarity

--- src/3D/main.ts
+++ src/3D/main.ts
@@ -6,37 +6,70 @@ export class RENDERER {
     scene: THREE.Scene
     camera: THREE.PerspectiveCamera
     renderer: THREE.WebGLRenderer
-    element: Element | null
     controls: OrbitControls
 
     constructor() {
-        
-        // Initialize Scene
         this.scene = new THREE.Scene();
-
         this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-        this.camera.position.z = 5
-
         this.renderer = new THREE.WebGLRenderer({antialias: true})
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+        // Resize screen 
+        window.addEventListener('resize', () => {
+            this.renderer.setSize(window.innerWidth, window.innerHeight);
+            this.camera.aspect = window.innerWidth / window.innerHeight;
+            
+            this.camera.updateProjectionMatrix()
+        })
+        
+        this.initialize()
+        this.addLights()
+        this.addObjects()
+        this.initControls()
+        this.render()
+    }
+
+    render = () => {
+        window.requestAnimationFrame(this.render);
+        this.controls.update()
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    initialize = () => {
+        this.camera.position.set(8, 8, 8)
+
         this.renderer.setClearColor("#e5e5e5")
         this.renderer.setSize(window.innerWidth, window.innerHeight)
-
+        this.renderer.shadowMap.enabled = true
         // Insert scene to DOM
-        this.element = document.getElementById('canavs-id')
-        if (this.element) this.element.appendChild(this.renderer.domElement)
+        let element = document.getElementById('canavs-id')
+        if (element) element.appendChild(this.renderer.domElement)
+    }
 
-        // Intialize Orbit Controls
-        this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    addLights = () => {
+        let light = new THREE.PointLight(0xFFFFFF, 1, 1000)
+        light.position.set(0,0,0)
+        light.castShadow = true
+        this.scene.add(light)
+
+        light = new THREE.PointLight(0xFFFFFF, 2, 0)
+        light.position.set(0,0,25)
+        light.castShadow = true
+        this.scene.add(light)
+    }
+
+    initControls = () => {
         this.controls.enableDamping = true
         this.controls.dampingFactor = 0.25
-        this.controls.enableZoom = false
         this.controls.enablePan = true
         this.controls.enableZoom = true
+        this.controls.autoRotate = true
 
         this.controls.minPolarAngle = 0;
         this.controls.maxPolarAngle = Math.PI / 2;
+    }
 
-        // Add objects
+    addObjects = () => {
         let geometry = new THREE.BoxGeometry(1, 1, 1)
         let material = new THREE.MeshLambertMaterial({color: 0xf7f7f7 })
 
@@ -47,28 +80,5 @@ export class RENDERER {
             mesh.position.z = (Math.random() - 0.5)*10
             this.scene.add(mesh)
         }
-
-        // Add Lights
-        let light = new THREE.PointLight(0xFFFFFF, 1, 1000)
-        light.position.set(0,0,0)
-        this.scene.add(light)
-        light = new THREE.PointLight(0xFFFFFF, 2, 1000)
-        light.position.set(0,0,25)
-        this.scene.add(light)
-
-        // Resize screen 
-        window.addEventListener('resize', () => {
-            this.renderer.setSize(window.innerWidth, window.innerHeight);
-            this.camera.aspect = window.innerWidth / window.innerHeight;
-
-            this.camera.updateProjectionMatrix()
-        })
-        
-        this.render()
-    }
-
-    render = () => {
-        window.requestAnimationFrame(this.render);
-        this.renderer.render(this.scene, this.camera);
     }
 }

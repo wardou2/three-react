b4a1e453ff6ef496008271305d50049db634cfae
wardou2 <ward92@gmail.com>
2020-09-01 14:43:26 -0400

    Change objects over to Points and Animate
    Using Vector3 and Points.
    credit to https://stackoverflow.com/questions/43845399/how-to-make-an-animated-wave-in-threejs for animation.

--- src/3D/main.ts
+++ src/3D/main.ts
@@ -1,7 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { TimelineMax, Expo} from 'gsap'
-import { request } from 'http';
+import { Geometry, Points, PointsMaterial } from 'three';
 
 
 export class RENDERER {
@@ -11,16 +11,30 @@ export class RENDERER {
     controls: OrbitControls
     raycaster: THREE.Raycaster
     mouse: THREE.Vector2
+    material: PointsMaterial
+    particleGeometry: Geometry
+    particleCloud: any
     objects: THREE.Mesh[]
+    theta: number
+    WIDTH: number
+    HEIGHT: number
+    SEPARATION: number
 
     constructor() {
         this.scene = new THREE.Scene();
-        this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+        this.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 2000)
         this.renderer = new THREE.WebGLRenderer({antialias: true})
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
         this.raycaster = new THREE.Raycaster()
         this.mouse = new THREE.Vector2()
+        this.material = new PointsMaterial({color: 0xf7f7f7 })
+        this.particleGeometry = new Geometry()
+        this.particleCloud = null
         this.objects = []
+        this.theta = 0
+        this.WIDTH = 100
+        this.HEIGHT = 100
+        this.SEPARATION = 8
 
         // Resize screen 
         window.addEventListener('resize', () => {
@@ -32,14 +46,34 @@ export class RENDERER {
         
         
         this.initialize()
-        this.addLights()
-        this.addObjects()
+        this.addParticleGeometry()
         this.initControls()
         this.render()
-        // window.addEventListener('mousemove', this.onMouseMove)
+        window.addEventListener('mousemove', this.onMouseMove)
     }
 
     render = () => {
+
+        // https://stackoverflow.com/questions/43845399/how-to-make-an-animated-wave-in-threejs
+        let index = 0
+        let time = Date.now() * 0.00005
+        let h = (360 * (1.0 + time) % 360) / 360
+        this.material.color.setHSL(h, 0.5, 0.5)
+
+        this.theta += 0.0008
+        
+        if (this.particleCloud) {
+            for (let x=0; x < this.WIDTH; x++) {
+                for (let z =0; z < this.HEIGHT; z++) {
+                    this.particleCloud.geometry.vertices[index].y = (
+                        Math.cos((x / this.WIDTH * Math.PI * 8 + this.theta)) + 
+                        Math.sin((z / this.HEIGHT * Math.PI * 8 + this.theta))
+                    )*10
+                    index++
+                }
+            }
+            this.particleCloud.geometry.verticesNeedUpdate = true
+        }
         window.requestAnimationFrame(this.render);
         this.controls.update()
 
@@ -47,7 +81,7 @@ export class RENDERER {
     }
 
     initialize = () => {
-        this.camera.position.set(0,0,100)
+        this.camera.position.set(400, 50, 400)
 
         this.renderer.setClearColor("#202020")
         this.renderer.setSize(window.innerWidth, window.innerHeight)
@@ -83,19 +117,33 @@ export class RENDERER {
         let geometry = new THREE.SphereGeometry(.1, 9, 9)
         let material = new THREE.MeshLambertMaterial({color: 0xf7f7f7 })
 
-        for (let x=-10; x<10; x++) {
-            for (let y=-10; y<10; y++) {
-                for (let z=-10; z<0; z++) {
+        for (let x=-50; x<50; x++) {
+            for (let y=-50; y<50; y++) {
+                // for (let z=-10; z<0; z++) {
                 let mesh = new THREE.Mesh(geometry, material)
-                    mesh.position.x = (x)*10
-                    mesh.position.y = (y)*10
-                    mesh.position.z = (z)*10
-                    // mesh.position.z = 0
+                    mesh.position.x = (x)*2
+                    mesh.position.y = (y)*2
+                    // mesh.position.z = (z)*10
+                    mesh.position.z = 0
                     this.objects.push(mesh)
                     this.scene.add(mesh)
-                }
+                // }
             }
         }
+    }
+
+    addParticleGeometry = () => {
+        for (let x = 0; x < this.WIDTH; x++) {
+            for (let z = 0; z < this.HEIGHT; z++) {
+                let vert = new THREE.Vector3()
+                vert.x = x * this.SEPARATION - ((this.WIDTH * this.SEPARATION) / 2)
+                vert.y = (Math.cos(x / this.WIDTH) * Math.PI * 6) + Math.sin((z / this.HEIGHT) * Math.PI * 6)
+                vert.z = z * this.SEPARATION - ((this.HEIGHT * this.SEPARATION) / 2)
+                this.particleGeometry.vertices.push(vert)
+            } 
+        }
+        this.particleCloud = new Points(this.particleGeometry, this.material)
+        this.scene.add(this.particleCloud)
     }
 
     onMouseMove = (event: MouseEvent) => {
@@ -107,11 +155,13 @@ export class RENDERER {
         this.raycaster.setFromCamera(this.mouse, this.camera);
 
         const intersects = this.raycaster.intersectObjects(this.scene.children, true);
-
+        console.log(intersects)
         for (let i=0; i < intersects.length; i++) {
-            let tl = new TimelineMax().delay(.3)
-            tl.to(intersects[i].object.scale, 1, {x: 2, ease: Expo.easeOut})
-            tl.to(intersects[i].object.scale, 1, {y: 2, ease: Expo.easeOut}, "=-1")
+            // let tl = new TimelineMax().delay(.3)
+            // tl.to(intersects[i].object.scale, 2, {x: 3, ease: Expo.easeOut})
+            // tl.to(intersects[i].object.scale, 2, {y: 3, ease: Expo.easeOut}, "=-2")
+            // tl.to(intersects[i].object.scale, 2, {x: 1, ease: Expo.easeOut}, "=2")
+            // tl.to(intersects[i].object.scale, 2, {y: 1, ease: Expo.easeOut}, "=-2")
         }
     }
 }
